f43e182090bf3a55fa336a87abc48d8160f79289
416Shemi <tu7ngcsvr@code.edu.az>
2022-03-08 02:29:28 +0400

    new projekt4

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NumLenght(23423));
+            Console.ReadLine();
+        }
+        static int NumLenght(int num)
+        {
+            int cont = 0;
+            while (num > 0)
+            {
+               num= num / 10;
+                cont++;
+            }
+            return cont;
+        }
+    }
+}
